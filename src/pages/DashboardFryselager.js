b0ac8575be02af8c839cb47142f9d742d71c9246
Anders Nystad Bendiksen <andersnystad@me.com>
2019-04-22 18:03:36 +0200

    Fixed double declaration

--- src/pages/DashboardFryselager.js
+++ src/pages/DashboardFryselager.js
@@ -272,7 +272,6 @@ class DashboardFryselager extends React.Component {
         }
 
         // Legger til annotations ved gitte variabler
-        let fryserVarsel = []
 
         fryserData.forEach(function(element, index){
             if (element.Temperatur > -15) {
